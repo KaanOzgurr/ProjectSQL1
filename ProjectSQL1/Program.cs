36002ec82f9b4f1c35382c0b3a1450eef807046c
Kaan Ozgur <165286662+KaanOzgurr@users.noreply.github.com>
2025-04-12 08:49:53 +0300

    Update Program.cs

--- ProjectSQL1/Program.cs
+++ ProjectSQL1/Program.cs
@@ -16,7 +16,7 @@ namespace ProjectSQL1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmLogin());
         }
     }
 }
